b78c32d6b1dd52bdb0f6b09295d62a114a6f4931
chris-costello6 <christopher.costello6@gmail.com>
2014-12-03 11:40:26 -0700

    use moodle libraries

--- PerMessage.h
+++ PerMessage.h
@@ -8,8 +8,8 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#include "Message.h"
-#include "threadPool.h"
+#include "newLibs/message.cpp"
+#include "newLibs/threadpool.cpp"
 #include "Headers.h"
 #include "udpUtils.h"
 
@@ -66,11 +66,12 @@ PerMessage::ethernetSend(int protocol, Message* mesg)
 	
 	// cout << "ethHeader: hlp=" << eth.hlp << " other=" << eth.otherInfo
 	// << " length=" << eth.length << endl;
-	// cout << "size of mesg before=" << mesg->msgLen() << endl;
 	// char content[30];
 	// mesg->msgFlat(content);
 	// cout << "content is " << content << " before sending" << endl;
 	
+	cout << "size of mesg before=" << mesg->msgLen() << endl;
+
 	mesg->msgAddHdr((char*)&eth, sizeof(ethHeader));
 	
 
@@ -86,10 +87,12 @@ PerMessage::ethernetSend(int protocol, Message* mesg)
 
 	// Send message over network
 	const char* destHost = "localhost";
-	char* buffer = new char[mesg->msgLen()];
+	char* buffer = new char[1024];
 	mesg->msgFlat(buffer);
-	for(int i = 0; i < mesg->msgLen(); i++) printf("%c\n", buffer[i]);
-	cout << "attempting to send " << buffer<< " to " << destHost << " port " << destUdpPort << endl;
+	// for(int i = 0; i < mesg->msgLen(); i++) buffer[i] = 't';
+		// printf("%c\n", buffer[i]);
+	cout << "attempting to send " << buffer<< " to " << destHost
+	 << " port " << destUdpPort << endl;
 	// int sendSock = udpSocket(inUdpPort);
 	sendUdp(mySock, buffer, mesg->msgLen(), destHost, destUdpPort);
 }
@@ -97,12 +100,13 @@ PerMessage::ethernetSend(int protocol, Message* mesg)
 void
 PerMessage::ethernetRecv(void* arg)
 {
-	cout << "ethernetRecv: Got mesg" << endl;
-
-	Message* mesg = (Message *) arg;
+	// cout << "ethernetRecv: Got mesg" << endl;
+	// char* xx= (char*) arg;
+	// cout << "content =" << xx << endl;
+	Message* mesg = (Message*) arg;
 	char* content = new char[30];
 	mesg->msgFlat(content);
-	for(int i = 0; i < 30; i++) printf("%c\n", content[i]);
+	// for(int i = 0; i < 30; i++) printf("%c\n", content[i]);
 	// cout << "content=" << content <<" size before msgStripHdr=" << mesg->msgLen() << endl;
 
 	ethHeader* header = (ethHeader*) mesg->msgStripHdr(sizeof(ethHeader));
@@ -119,12 +123,18 @@ PerMessage::listenOnSocket(void* arg)
 	PerMessage* pm = (PerMessage*) arg;
 	cout << "Listening on port " << pm->inUdpPort << endl;
 	char* buffer = new char[BUFSIZE];
+	char* mm = new char[200];
+	// char* xx = new char[15];
 	while(true) {
 		memset(buffer, 0, BUFSIZE);	
 		int n = getUdpMesg(pm->mySock, buffer, BUFSIZE);
 		Message* m = new Message(buffer, n); //Only read in num bytes read
-		// cout << "got message " << raw << " size=" << strlen(raw)<< endl;
-		pm->threads->dispatch_thread(PerMessage::ethernetRecv, (void*)m);
+		m->msgFlat(mm);
+		cout << "n=" << n << " mesgLen=" << m->msgLen() << endl;
+		for(int i = 0; i < 30; i++) printf("%c\n", mm[i]);
+		// cout << "passing in new char* instead of message." << endl;
+		// for (int i = 0; i < 15; ++i) xx[i] = 'x';
+		pm->threads->dispatch_thread(PerMessage::ethernetRecv, (void*) m);
 	}
 }
 
